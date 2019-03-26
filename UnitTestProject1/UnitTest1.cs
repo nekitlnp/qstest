bae7d9eca72958d7d6fb60f6d4db6aaa06ea49ff
Никита Паренюк <parenyuk2014@gmail.com>
2019-03-26 12:29:16 +0500

    QuickSort выкладываю нормально, не архивом)))

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using ClassLibrary1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void Test3()
+        {
+            var array = new int[] { 1, 2, 3 };
+            FastQuick.QuickSort(array);
+            Shield(array);
+        }
+
+        [TestMethod]
+        public void Test100()
+        {
+            var rnd = new Random(42);
+            var array = new int[100];
+            for (var i = 0; i < array.Length - 1; i++)
+                array[i] = rnd.Next(-100, 100);
+            FastQuick.QuickSort(array);
+            Shield(array);
+        }
+
+        [TestMethod]
+        public void Test1000()
+        {
+            var rnd = new Random(42);
+            var array = new int[1000];
+            for (var i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(-100, 100);
+            FastQuick.QuickSort(array);
+            for (var i = 0; i < 10; i++)
+            {
+                var j = rnd.Next(0, array.Length - 2);
+                var k = rnd.Next(0, array.Length - 1);
+                if (k >= j)
+                    NUnit.Framework.Assert.GreaterOrEqual(array[k], array[j]);
+                else
+                    NUnit.Framework.Assert.GreaterOrEqual(array[j], array[k]);
+            }
+            Shield(array);
+        }
+
+        [TestMethod]
+        public void Test0()
+        {
+            FastQuick.QuickSort(new int[0]);
+        }
+
+        [TestMethod]
+        public void Test300M()
+        {
+            var rnd = new Random(42);
+            var array = new int[300000000];
+            for (var i = 0; i < array.Length; i++)
+                array[i] = rnd.Next(int.MinValue, int.MaxValue);
+            FastQuick.QuickSort(array);
+            Shield(array);
+        }
+
+        public void Shield(int[] arr)
+        {
+            for (var i = 0; i < arr.Length - 1; i++)
+            {
+                NUnit.Framework.Assert.GreaterOrEqual(arr[i + 1], arr[i]);
+            }
+        }
+    }
+}
--- qstest/Class1.cs
+++ qstest/Class1.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace qstest
+﻿namespace ClassLibrary1
 {
-    public class Class1
+    public class FastQuick
     {
+        public static void QuickSort(int[] array)
+        {
+            if (array is null) return;
+            Sorting(array, 0, array.Length - 1);
+        }
+        public static void Sorting(int[] array, int first, int last)
+        {
+            if (last <= 1) return;
+            int p = array[(last - first) / 2 + first];
+            int temp;
+            int i = first;
+            int j = last;
+
+            while (i < j)
+            {
+                while (array[i] < p && i < last)
+                {
+                    ++i;
+                }
+
+                while (array[j] > p && j > first)
+                {
+                    --j;
+                }
+
+                if (i <= j)
+                {
+
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    ++i;
+                    --j;
+                }
+            }
+            if (j > first)
+                Sorting(array, first, j);
+            if (i < last)
+                Sorting(array, i, last);
+        }
     }
 }
